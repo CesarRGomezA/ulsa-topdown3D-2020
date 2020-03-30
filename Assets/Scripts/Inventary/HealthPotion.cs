32255e1bf8a5aa943de5e1ffabffc14ce0498edd
CesarRGomezA <CesarRGomezA@gmail.com>
2020-03-30 15:14:04 -0700

    Resubido, ya funcionan las pociones

---
+++ Assets/Scripts/Inventary/HealthPotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+   [SerializeField]
+
+   Potion potion;
+
+    //no es lo mas optimo por si tienes varias posiones ya que todas escucharan este comportamiento
+   private void OnTriggerEnter(Collider other) 
+   {
+       potion.Drink();
+       Debug.Log("Healt Restored");
+       Destroy(gameObject);
+   }
+}
