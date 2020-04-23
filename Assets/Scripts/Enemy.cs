87c2599cff5c29334721d5d5eff75566965956d5
CesarRGomezA <CesarRGomezA@gmail.com>
2020-04-23 14:46:18 -0700

    nsjaksn

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField, Range(0.1f, 10f)]
     float moveSpeed;
     [SerializeField, Range(0.1f, 10f)]
     float minDistance;
+
+    NavMeshAgent navMeshAgent;
+
+    void Awake() 
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
     void Update() 
     {
         if(Attack)
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            navMeshAgent.destination = Gamemanager.instance.Player.transform.position;
             transform.LookAt(Gamemanager.instance.Player.transform);
         }
     }
