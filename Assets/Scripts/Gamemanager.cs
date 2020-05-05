ccc14d13f3ebf4af8d374f414186795f84d0dcfc
CesarRGomezA <CesarRGomezA@gmail.com>
2020-05-05 15:30:01 -0700

    Audio de combate y Audio de fondo

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -11,8 +11,12 @@ public class Gamemanager : MonoBehaviour
 
      bool isInCombat;
 
+    [SerializeField]
+     GameAudio gameAudio;
+
     public Player Player { get => player; }
     public bool IsInCombat { get => isInCombat; set => isInCombat = value; }
+    public GameAudio GameAudio { get => gameAudio; }
 
     void Awake() 
     {
@@ -27,10 +31,17 @@ public class Gamemanager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    void Start() 
+    {
+        gameAudio.Aud = GetComponent<AudioSource>();
+        gameAudio.PlayBGMusic();
+    }
+
     public void StartCombat()
     {
         player.Anim.SetLayerWeight(1, 1);
         player.WeaponVisible(true);
+        gameAudio.PlaBattleMusic();
     }
 
      public void StopCombat()
@@ -38,5 +49,6 @@ public class Gamemanager : MonoBehaviour
         player.Anim.SetLayerWeight(0, 1);
         player.Anim.SetLayerWeight(1, 0);
         player.WeaponVisible(false);
+        gameAudio.PlayBGMusic();
     }
 }
